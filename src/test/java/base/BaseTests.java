e23804fa0fcb9542468c94906e9d9ce9281f8305
BenjaminAliriza <blaliriza@gmail.com>
2022-06-22 15:53:14 +0200

    pages established in framework

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,27 +5,24 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 public class BaseTests {
     private WebDriver driver;
 
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
-
+    protected HomePage homePage;
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        WebElement inputsLink =  driver.findElement(By.xpath("//a[@href='/inputs']"));
-        inputsLink.click();
+        homePage = new HomePage(driver);
 
-//      driver.manage().window().fullscreen(); - maximize does the same.
-//      driver.manage().window().setSize(new Dimension(375 ,812)); - this way allows a specific size to be set
-
-        System.out.println(driver.getTitle());
         driver.quit(); //driver.close would close window, while driver.quit ends session and closes everything.
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
     }
 }
--- src/test/java/exercises/TheInternetShiftingContent.java
+++ src/test/java/exercises/TheInternetShiftingContent.java
@@ -1,6 +1,5 @@
 package exercises;
 
-import base.BaseTests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,26 +10,28 @@ import java.util.List;
 public class TheInternetShiftingContent {
 
     private WebDriver driver;
-    private WebElement shiftingContent =  driver.findElement(By.xpath("//a[@href='/shifting_content']"));
-    private WebElement menuElement =  driver.findElement(By.linkText("Menu Element"));
-    private List<WebElement> menuElements =  driver.findElements(By.xpath("//a"));
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TheInternetShiftingContent TISC = new TheInternetShiftingContent();
         TISC.countMenuElements();
     }
 
-    public void countMenuElements(){
+    public void countMenuElements() throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
+        WebElement shiftingContent =  driver.findElement(By.xpath("//a[@href='/shifting_content']"));
         shiftingContent.click();
-        menuElement.click();
-        System.out.println(menuElements.size());
 
+        WebElement menuElement =  driver.findElement(By.xpath("//a[@href='/shifting_content/menu']"));
+        menuElement.click();
+
+        List<WebElement> menuElements =  driver.findElements(By.xpath("//li"));
+        System.out.println(menuElements.size());
+        driver.quit();
 
     }
 
