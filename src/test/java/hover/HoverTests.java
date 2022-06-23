1f6eb6da8f6d85a9fd48a595a61c36bf894832e3
BenjaminAliriza <blaliriza@gmail.com>
2022-06-23 15:59:18 +0200

    rush!

---
+++ src/test/java/hover/HoverTests.java
@@ -0,0 +1,22 @@
+package hover;
+
+import base.BaseTests;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class HoverTests extends BaseTests {
+
+    @Test
+    public void testHoverUser1(){
+        var hoversPage = homePage.clickHovers();
+        var caption = hoversPage.hoverOverFigure(1);
+        assertTrue(caption.isCaptionDisplayed(), "Caption not displayed");
+        assertEquals(caption.getTitle(), "name: user1", "Caption is incorrect");
+        assertEquals(caption.getLinkText(), "View profile", "Caption is incorrect");
+        assertTrue(caption.getLink().endsWith("/users/1"), "Link incorrect");
+    }
+
+}
