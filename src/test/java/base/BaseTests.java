f03e8c52b9c8126a9238bc56dd4a8b16cdf2a4a5
Benjamin Aliriza <baliriza@dvt.co.za>
2022-07-01 11:27:29 +0200

    tried to add screenshot on failure, need to revisit with Matt?

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,22 +1,21 @@
 package base;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
 import utils.WindowManager;
 
+import javax.xml.transform.Result;
+import java.io.File;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseTests {
     private WebDriver driver;
-
     protected HomePage homePage;
+
     @BeforeAll
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
@@ -30,12 +29,20 @@ public class BaseTests {
         driver.get("https://the-internet.herokuapp.com/");
 
     }
-
     @AfterAll
     public void tearDown(){
         driver.quit();//driver.close would close window, while driver.quit ends session and closes everything.
     }
-
+//    @AfterEach                try using this website later to get something working (https://www.baeldung.com/junit-testwatcher)
+//    public void recordFailure(){
+//        var camera = (TakesScreenshot)driver;
+//        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+//        try {
+//        Files.move(screenshot, new File("resources/screenshots/test.png"));
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
     public WindowManager getWindowManager(){
         return new WindowManager(driver);
     }
